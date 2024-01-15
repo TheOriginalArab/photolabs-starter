a46e709dcd4715be931f674a5b419a179081d727
Peter Hashem <peterhashem@gmail.com>
2024-01-15 16:22:46 -0500

    replaced TopNavigation and PhotoList with HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import React from "react";
 import "./App.scss";
 //import TopicList from "components/TopicList";
 //import TopNavigation from "components/TopNavigationBar";
-import HomeRoute from "components/HomeRoute";
+import HomeRoute from "routes/HomeRoute";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
