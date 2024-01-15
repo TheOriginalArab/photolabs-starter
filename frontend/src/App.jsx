b7f871a7c5d13f3e0bb0dad4f5978393ccac2c54
Peter Hashem <peterhashem@gmail.com>
2024-01-15 16:07:24 -0500

    Added TopNavigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,15 @@ import React from "react";
 //import PhotoListItem from "./components/PhotoListItem";
 import PhotoList from "components/PhotoList";
 import "./App.scss";
-import TopicList from "components/TopicList";
+//import TopicList from "components/TopicList";
+import TopNavigation from "components/TopNavigationBar";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {/* {photoListItems.map((data) => (
-        <PhotoListItem key={data.id} data={data} />
-      ))} */}
-      <TopicList />
+      <TopNavigation />
+      {/* <TopicList /> */}
       <PhotoList />
     </div>
   );
