308baaa274a5cb16205c33e28643f11abddd7602
Peter Hashem <peterhashem@gmail.com>
2024-01-15 16:21:07 -0500

    added TopNavigation and PhotoList to HomeRoute

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-
-import '../styles/HomeRoute.scss';
+import React from "react";
+import TopNavigation from "./TopNavigationBar";
+import PhotoList from "./PhotoList";
+import "../styles/HomeRoute.scss";
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation />
+      <PhotoList />
     </div>
   );
 };
