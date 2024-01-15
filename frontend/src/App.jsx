414005eebf5cb65d0f9b169a22b931742db5a2a4
Peter Hashem <peterhashem@gmail.com>
2024-01-14 19:54:03 -0500

    added 3 photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
+import PhotoListItem from "./components/PhotoListItem";
+import "./App.scss";
 
 const sampleDataForPhotoListItem = {
   id: "1",
@@ -14,12 +14,15 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const photoListItems = new Array(3).fill(sampleDataForPhotoListItem);
+
   return (
     <div className="App">
-      <PhotoListItem data={sampleDataForPhotoListItem}/>
+      {photoListItems.map((data, index) => (
+        <PhotoListItem key={index} data={data} />
+      ))}
     </div>
   );
 };
