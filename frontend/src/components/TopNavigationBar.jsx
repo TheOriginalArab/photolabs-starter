093263657bb2d9b48270b604f2c1b0bec335b94c
Peter Hashem <peterhashem@gmail.com>
2024-01-15 17:13:01 -0500

    using props to pass topics into TopicList

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,13 +3,12 @@ import React from "react";
 import "../styles/TopNavigationBar.scss";
 import TopicList from "./TopicList";
 import FavBadge from "./FavBadge";
-import topics from "mocks/topics";
 
-const TopNavigation = () => {
+const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics} />
+      <TopicList topics={props.topics} />
       <FavBadge />
     </div>
   );
