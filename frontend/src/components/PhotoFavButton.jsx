56474dd2c3136c7ed89f21a9c66a8e3d5d3deb39
Peter Hashem <peterhashem@gmail.com>
2024-01-14 20:39:23 -0500

    added useState

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,20 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import FavIcon from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import FavIcon from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton() {
+  const [isFav, setIsFav] = useState(false);
+
+  const toggleFav = useCallback(() => {
+    setIsFav((prev) => !prev);
+  }, []);
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={toggleFav}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        {/* Render the FavIcon component and pass the isFav state */}
+        <FavIcon isFav={isFav} />
       </div>
     </div>
   );
