97a8c46c5d9dfb40572206c6c739f2e92a302a65
Peter Hashem <peterhashem@gmail.com>
2024-01-14 21:08:38 -0500

    made to accept props from PhotoList

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,22 +6,30 @@ import PhotoFavButton from "./PhotoFavButton";
 const PhotoListItem = ({ data }) => {
   /* Insert React */
 
-  //const {data} = props;
+  const {
+    location: { city, country },
+    urls: { regular },
+    user: { username, name, profile },
+  } = data;
 
   return (
     <div className="photo-list__item">
       <PhotoFavButton />
       <img
-        src={data.imageSource}
-        alt={data.location.city + " photo"}
-        className="photo"
+        src={regular}
+        alt={`Photo Taken in ${city}, ${country} by ${name} (@${username})`}
+        className="photo-list__image"
       />
-      <div className="photo-info">
-        <img src={data.profile} alt={data.username} className="user-profile" />
-        <div className="user-info">
-          <div className="user-name">{data.username}</div>
-          <div className="location">
-            {data.location?.city}, {data.location?.country}
+      <div className="photo-list__user-details">
+        <img
+          src={profile}
+          alt={`${name}'s profile`}
+          className="photo-list__user-profile"
+        />
+        <div className="photo-list__user-info">
+          <div className="photo-list__user-info">{username}</div>
+          <div className="photo-list__user-location">
+            {`${city}, ${country}`}
           </div>
         </div>
       </div>
