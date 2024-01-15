57b1410b17ce95aab7f657dd363af9d773d97d8f
Peter Hashem <peterhashem@gmail.com>
2024-01-15 16:55:11 -0500

    imported topics and used to populate TopicListItem

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,32 +1,32 @@
 import React from "react";
-
+import topics from "mocks/topics";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+// const sampleDataForTopicList = [
+//   {
+//     id: "1",
+//     slug: "topic-1",
+//     title: "Nature",
+//   },
+//   {
+//     id: "2",
+//     slug: "topic-2",
+//     title: "Travel",
+//   },
+//   {
+//     id: "3",
+//     slug: "topic-3",
+//     title: "People",
+//   },
+// ];
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
       {/* Insert React */}
-      {sampleDataForTopicList.map((data) => (
-        <TopicListItem key={data.id} data={data} />
+      {topics.map((topic) => (
+        <TopicListItem key={topic.id} data={topic} />
       ))}
     </div>
   );
