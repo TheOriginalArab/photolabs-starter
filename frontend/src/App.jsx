f1ada343b6c510fc7079c7178230edb987d44249
Peter Hashem <peterhashem@gmail.com>
2024-01-14 21:38:56 -0500

    added TopicList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,16 @@ import React from "react";
 //import PhotoListItem from "./components/PhotoListItem";
 import PhotoList from "components/PhotoList";
 import "./App.scss";
-
-// const sampleDataForPhotoListItem = {
-//   id: "1",
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//   username: "Joe Example",
-//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-// };
+import TopicList from "components/TopicList";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  //const photoListItems = new Array(3).fill(sampleDataForPhotoListItem);
-
   return (
     <div className="App">
       {/* {photoListItems.map((data) => (
         <PhotoListItem key={data.id} data={data} />
       ))} */}
+      <TopicList />
       <PhotoList />
     </div>
   );
