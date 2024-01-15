8efd6191b3b498b3ed68f9dd11fb11d3aefe0d92
Peter Hashem <peterhashem@gmail.com>
2024-01-15 17:58:42 -0500

    fixed an error while passing props to TopicListItem

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -6,7 +6,7 @@ const TopicList = (props) => {
   return (
     <div className="top-nav-bar__topic-list">
       {props.topics.map((topic) => (
-        <TopicListItem key={topic.id} label={topic.title} />
+        <TopicListItem key={topic.id} label={topic} />
       ))}
     </div>
   );
