bb124ebb8d2f0ce5081b828b892f6b5437058949
Peter Hashem <peterhashem@gmail.com>
2024-01-15 17:10:25 -0500

    import mock data and passed into HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-
-//import PhotoListItem from "./components/PhotoListItem";
-//import PhotoList from "components/PhotoList";
 import "./App.scss";
-//import TopicList from "components/TopicList";
-//import TopNavigation from "components/TopNavigationBar";
 import HomeRoute from "routes/HomeRoute";
+import mockPhotoData from "mocks/photos";
+import mockTopicData from "mocks/topics";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute photos={mockPhotoData} topics={mockTopicData}/>
     </div>
   );
 };
