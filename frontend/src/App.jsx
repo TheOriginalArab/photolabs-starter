20c037c5e18ba8887920ea878f17b0076fcdde97
Peter Hashem <peterhashem@gmail.com>
2024-01-15 16:17:41 -0500

    removed components & replaced with HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
 //import PhotoListItem from "./components/PhotoListItem";
-import PhotoList from "components/PhotoList";
+//import PhotoList from "components/PhotoList";
 import "./App.scss";
 //import TopicList from "components/TopicList";
-import TopNavigation from "components/TopNavigationBar";
+//import TopNavigation from "components/TopNavigationBar";
+import HomeRoute from "components/HomeRoute";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopNavigation />
-      {/* <TopicList /> */}
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
