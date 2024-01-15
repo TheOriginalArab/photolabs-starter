22deb8fb35df6c95930e82a6c92416140fa95b0b
Peter Hashem <peterhashem@gmail.com>
2024-01-15 17:11:51 -0500

    using props to display topics

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,32 +1,12 @@
 import React from "react";
-import topics from "mocks/topics";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-// const sampleDataForTopicList = [
-//   {
-//     id: "1",
-//     slug: "topic-1",
-//     title: "Nature",
-//   },
-//   {
-//     id: "2",
-//     slug: "topic-2",
-//     title: "Travel",
-//   },
-//   {
-//     id: "3",
-//     slug: "topic-3",
-//     title: "People",
-//   },
-// ];
-
-const TopicList = () => {
+const TopicList = (props) => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
-      {topics.map((topic) => (
-        <TopicListItem key={topic.id} data={topic} />
+      {props.topics.map((topic) => (
+        <TopicListItem key={topic.id} label={topic.title} />
       ))}
     </div>
   );
