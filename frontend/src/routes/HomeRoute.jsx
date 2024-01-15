eb23639b970c103e41e4b02c271565f645bde0f1
Peter Hashem <peterhashem@gmail.com>
2024-01-15 16:53:15 -0500

    fixed imports for TopNav and PhotoList

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import TopNavigation from "./TopNavigationBar";
-import PhotoList from "./PhotoList";
+import TopNavigation from "components/TopNavigationBar";
+import PhotoList from "components/PhotoList";
 import "../styles/HomeRoute.scss";
 
 const HomeRoute = () => {
