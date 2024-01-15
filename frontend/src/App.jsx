d2a3b25c7ec5462712bffd64caedd8434e00f4bf
Peter Hashem <peterhashem@gmail.com>
2024-01-14 21:09:45 -0500

    displaying PhotoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 
-import PhotoListItem from "./components/PhotoListItem";
+//import PhotoListItem from "./components/PhotoListItem";
+import PhotoList from "components/PhotoList";
 import "./App.scss";
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photoListItems = new Array(3).fill(sampleDataForPhotoListItem);
+  //const photoListItems = new Array(3).fill(sampleDataForPhotoListItem);
 
   return (
     <div className="App">
-      {photoListItems.map((data, index) => (
-        <PhotoListItem key={index} data={data} />
-      ))}
+      {/* {photoListItems.map((data) => (
+        <PhotoListItem key={data.id} data={data} />
+      ))} */}
+      <PhotoList />
     </div>
   );
 };
