a361aaf07d77d370fc31df7dad630e54f2e7747f
Peter Hashem <peterhashem@gmail.com>
2024-01-16 17:42:17 -0500

    wrote functionality

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import '../styles/PhotoDetailsModal.scss'
-import closeSymbol from '../assets/closeSymbol.svg';
+import "../styles/PhotoDetailsModal.scss";
+import closeSymbol from "../assets/closeSymbol.svg";
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
