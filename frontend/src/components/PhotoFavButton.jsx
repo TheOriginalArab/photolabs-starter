72d595af3372f9d704d793d090dc5c2909466919
Peter Hashem <peterhashem@gmail.com>
2024-01-16 17:44:54 -0500

    fixed fav button functionality

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 
-//import FavIcon from "./FavIcon";
-import FavBadge from "./FavBadge";
+import FavIcon from "./FavIcon";
 import "../styles/PhotoFavButton.scss";
 
-function PhotoFavButton({ photos, toggleFavorite, photoId, favoritePhotos }) {
+function PhotoFavButton({ toggleFavorite, photoId, favoritePhotos }) {
   const handleClick = (photoId) => {
     toggleFavorite(photoId);
     console.log("click fired!", photoId);
   };
 
+  const selectedPhoto = favoritePhotos.includes(photoId);
+
   return (
     <div className="photo-list__fav-icon" onClick={() => handleClick(photoId)}>
       <div className="photo-list__fav-icon-svg">
-        <FavBadge 
-          photos={photos} 
-          photoId={photoId} 
-          favoritePhotos={favoritePhotos}
-        />
+        <FavIcon selectedPhoto={selectedPhoto} />
       </div>
     </div>
   );
