3a190c28b04e3bba76b1df2f2247a47ba8e33e21
Peter Hashem <peterhashem@gmail.com>
2024-01-16 17:41:34 -0500

    added modal upon photoclick

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
 import mockPhotoData from "mocks/photos";
 import mockTopicData from "mocks/topics";
 import { useState } from "react";
@@ -8,16 +9,28 @@ import { useState } from "react";
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   const [favoritePhotos, setFavoritePhotos] = useState([]);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleFavorite = (id) => {
-    console.log("toggleFavorite triggered!", id)
+    console.log("toggleFavorite triggered!", id);
     if (favoritePhotos.includes(id)) {
       return setFavoritePhotos(
         favoritePhotos.filter((favPhotoId) => favPhotoId !== id)
       );
     } else {
-      return setFavoritePhotos((prev) => prev.push(id));
+      return setFavoritePhotos((prev) => [...prev, id]);
     }
+  };
+
+  const handlePhotoClick = (photo) => {
+    setSelectedPhoto(photo);
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    setSelectedPhoto(null);
   };
 
   return (
@@ -27,7 +40,16 @@ const App = () => {
         topics={mockTopicData}
         favoritePhotos={favoritePhotos}
         toggleFavorite={toggleFavorite}
+        onPhotoClick={handlePhotoClick}
       />
+      <PhotoDetailsModal
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+      >
+        <div>
+          {selectedPhoto && <img src={selectedPhoto.urls.regular} />}
+        </div>
+      </PhotoDetailsModal>
     </div>
   );
 };
