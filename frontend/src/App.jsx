8d9412df5bbda145aac1e775a34c58ebbfd971f9
Peter Hashem <peterhashem@gmail.com>
2024-01-15 20:46:23 -0500

    added useState to Favorite photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,34 @@ import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
 import mockPhotoData from "mocks/photos";
 import mockTopicData from "mocks/topics";
+import { useState } from "react";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const [favoritePhotos, setFavoritePhotos] = useState({});
+
+  const toggleFavorite = (id) => {
+    setFavoritePhotos((prevFavoritePhotos) => {
+      const newFavorites = { ...prevFavoritePhotos };
+
+      if (newFavorites[id]) {
+        delete newFavorites[id];
+      } else {
+        newFavorites[id] = true;
+      }
+      //console.log(newFavorites);
+      return newFavorites;
+    });
+  };
+
   return (
     <div className="App">
-      <HomeRoute photos={mockPhotoData} topics={mockTopicData}/>
+      <HomeRoute
+        photos={mockPhotoData}
+        topics={mockTopicData}
+        favoritePhotos={favoritePhotos}
+        toggleFavorite={toggleFavorite}
+      />
     </div>
   );
 };
