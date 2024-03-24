a89af29487e289fe571ccded0ed8e31661cc8e7b
Peter Hashem <peterhashem@gmail.com>
2024-03-24 17:51:19 -0400

    fixed props

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,9 +3,7 @@ import FavIcon from "./FavIcon";
 
 import "../styles/FavBadge.scss";
 
-const FavBadge = ({ favoritePhotos, photoId }) => {
-  console.log("favoritePhotos", favoritePhotos);
-  console.log("photoId", photoId);
+const FavBadge = ({ favoritePhotos }) => {
   const displayAlert = favoritePhotos.length > 0;
   return (
     <div className="fav-badge">
