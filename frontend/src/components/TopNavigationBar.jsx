37533d1d02880ac99875328ba4c872e3edfca3fd
Peter Hashem <peterhashem@gmail.com>
2024-03-24 17:56:34 -0400

    add showAllPhotos function

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,17 +4,25 @@ import "../styles/TopNavigationBar.scss";
 import TopicList from "./TopicList";
 import FavBadge from "./FavBadge";
 
-const TopNavigation = ( { 
+const TopNavigation = ({
   photos,
   topics,
-  favoritePhotos
+  favoritePhotos,
+  fetchPhotosByTopic,
+  fetchAllPhotos,
 }) => {
-  
+  const showAllPhotos = (e) => {
+    e.preventDefault();
+    fetchAllPhotos();
+  };
+
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics} />
-      <FavBadge favoritePhotos={favoritePhotos} photos={photos}/>
+      <span className="top-nav-bar__logo" onClick={showAllPhotos}>
+        PhotoLabs
+      </span>
+      <TopicList topics={topics} fetchPhotosByTopic={fetchPhotosByTopic} />
+      <FavBadge favoritePhotos={favoritePhotos} photos={photos} />
     </div>
   );
 };
